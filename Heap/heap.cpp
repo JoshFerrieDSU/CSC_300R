8ba283b7f86d9eb8f61e2c217f49f6c5521c439e
Joshua Ferrie <josh.ferrie@trojans.dsu.edu>
2025-11-15 17:48:30 -0600

    Added and completed Heap

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -38,11 +38,6 @@ void HEAP::heapifyUp(int index) {
     }
 }
 
-void HEAP:: switchMinMax(){
-    
-}
-
-
 int HEAP::peek() {
     if (heapSize <= 0) {
         cout << "Heap is empty" << endl;
@@ -94,36 +89,173 @@ void HEAP::deleteH(int data) {
 
 void HEAP::heapifyDown(int index) 
 {
-    // complete this
+    while (true){
+        int left = leftChild(index);
+        int right = rightChild(index);
+        int parent = index;
+
+        if (left < heapSize && array[left] > array[parent]) //if in range and parent is LESS than left
+        {
+            parent = left;//set new max
+        }
+
+        if (right < heapSize && array[right] > array[parent])//in in range and parent is less than right
+        {
+            parent = right;//set new max
+        }
+
+        if (parent == index) //if parent = parent means nothing changed
+        {
+            break; //stop no need to continue and break the loop
+        }
+
+        swap(array[index], array[parent]); //helpful function that I never knew was in c so thanks
+        index = parent;
+    }
 }
 
  
 void HEAP::buildH(int arr[], int n) 
 {
-    // complete this
-}
+    if (n > capacity){//capacity check
+        cout<<"TOO MUCH INPUT!"<<endl;
+    }
+
+    for (int i = 0; i< n; i++){
+        array[i] = arr[i]; //set heap values from input
+    }
+    heapSize = n;//set size
+
+    if (heapSize > 0){
+        int start = parent(heapSize - 1); //start at last non leaf node
+        for (int i = start; 0 <= i; i--){
+            heapifyDown(i); //work your way up
+        }
+    }
+
+    for (int i = 0; i<n; i++){
+        arr[i] = array[i]; //finalize changes
+    }
+
+
+} 
 
 void HEAP::replace(int oldData, int newData)
 {
-    // complete this
+    int index = INT_MIN;
+
+    for (int i = 0; i< heapSize; i++){
+        if (array[i] == oldData){
+            index = i;
+            break; //go through list and find old data index
+        }
+    }
+
+    if (index == INT_MIN)//no changes
+    {
+        cout<<"Cant replace, doesn't exist"<<endl;
+        return;
+    }
+
+    int old = array[index];
+    array[index] = newData; //replace
+
+    if (newData > old){ //if value is larger then move it up if needed
+        heapifyUp(index);
+    } else if (newData < old){ //if value is smaller then move it down if needed
+        heapifyDown(index);
+    } //no need to consider equal
 }
 
-void HEAP::heapSort(int arr[], int n)
+void HEAP::heapSort(int arr[], int n)//sort array using heap small to large
 {
-    // complete this
+    if (n<=0){
+        return;
+    }
+
+    HEAP temp(n);
+    temp.buildH(arr, n);//make heap from array and it should sort it
+    
+    for (int i = n-1; 0 <= i; i--){
+        int max = temp.peek();
+        temp.deleteMax();
+        arr[i] = max;
+    }
+
+    for (int i = 0; i<n; i++){
+        cout <<arr[i]<<" ";
+    }
+    cout<<endl;
 }
 
-void HEAP::printHeap(int index, int depth) {
+void HEAP::heapifyDownMin(int index){
+    while (true){
+        int left = leftChild(index);
+        int right = rightChild(index);
+        int parent = index; //min
+
+        if (left < heapSize && array[left] < array[parent]) //if in range and parent is more than left
+        {
+            parent = left;//set new min
+        }
+
+        if (right < heapSize && array[right] < array[parent])//in in range and parent is more than right
+        {
+            parent = right;//set new min
+        }
+
+        if (parent == index) //if parent = parent means nothing changed
+        {
+            break; //stop no need to continue and break the loop
+        }
+
+        swap(array[index], array[parent]); //helpful function that I never knew was in c so thanks
+        index = parent;
+    }
+}
+
+void HEAP::switchMinMax(){
+    if (heapSize <= 1){
+        // if there is only less than 1 element then what's the point
+        return;
+    }
+
+    int left = leftChild(0);
+    int ismax = true;
+
+    if (left < heapSize){
+        if (array[0] < array[left]){
+            ismax = false; // root is less than child
+        }
+    }
+
+    int start = parent(heapSize - 1);
+    if (ismax){ //if max heap then convert to min using hdm
+        for (int i = start; 0 <= i; i--){
+            heapifyDownMin(i); 
+        }
+    } else{ //if min heap then convert to max using old way
+        for (int i = start; 0 <= 0; i--){
+            heapifyDown(i);
+        }
+    }
+}
+
+void HEAP::printHeap(int index, int depth) {//old print wasn't working so I had it just print the array.
     if (index >= heapSize)
         return;
 
-    printHeap(rightChild(index), depth + 1);
+        for (int i = 0; i<=heapSize-1; i++){
+            cout<<array[i]<<" ";
+        }
+        cout<<endl;
+    // printHeap(rightChild(index), depth + 1);
 
-    for (int i = 0; i < depth; i++) {
-        cout << "  "; // indentation based on depth
-    }
+    // for (int i = 0; i < depth; i++) {
+    //     cout << "    "; // indentation based on depth
+    // }
 
-    cout << array[index] << endl;
+    // cout << array[index] << endl;
 
-    printHeap(leftChild(index), depth + 1);
+    // printHeap(leftChild(index), depth + 1);
 }
--- Heap/heap.h
+++ Heap/heap.h
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <utility>
 
 using namespace std;
 
